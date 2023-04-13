14b6b5308417e5991e59050747d26dae9d9df306
GoncaloPereiraFigueiredoFerreira <a93218@alunos.uminho.pt>
2023-04-13 15:21:12 +0100

    ArchivalSystem base

---
+++ ArchivalSystem/app.js
@@ -0,0 +1,38 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+require("dotenv").config();
+
+
+var ingestRouter= require('./routes/ingest');
+var acessRouter = require('./routes/acess');
+var manageRouter= require('./routes/manage');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/ingest', ingestRouter);
+app.use('/acess', acessRouter);
+app.use('/manage', manageRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // send error status
+  res.sendStatus(err.status || 500);
+
+});
+
+module.exports = app;
