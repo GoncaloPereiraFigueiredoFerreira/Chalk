0ddde78f1f7f770f68d585dab2d616a96cc53b31
GoncaloPereiraFigueiredoFerreira <a93218@alunos.uminho.pt>
2023-04-13 15:47:52 +0100

    Base for the Chalk API

---
+++ Chalk/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./views/*.{pug, html}'],
+  theme: {
+    extend: {
+    },
+    fontFamily:{
+      'pacifico': ['Pacifico','sans-serif']
+    }
+  },
+  variants:{
+    extend:{}
+  },
+  plugins: [
+  ],
+}
+
