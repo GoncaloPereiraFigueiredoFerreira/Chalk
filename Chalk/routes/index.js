3c54b262fa243e19d4add7a809650745431eefaa
RayMightBeWrong <ruizinho7braga@gmail.com>
2023-04-22 19:46:42 +0100

    bag it

--- Chalk/routes/index.js
+++ Chalk/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var multer = require('multer')
+var bagit = require('../../BagIt/bagit')
+
+var upload = multer({dest: 'uploads'})
+
+const uploadFolder = 'uploads'
+
+
+//var fs = require('fs')
+var archiver = require('archiver')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('channel');
+    res.render('channel');
 });
+
+router.post('/files', upload.single('myFile'), (req, res) => {
+    var archive = archiver('zip', {zlib: {level: 9}})
+    bagit.create_bag(__dirname + '/../' + uploadFolder, 'target.zip', archive, req)
+    res.redirect('/')
+})
 
 module.exports = router;
